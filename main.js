d154e92b0949c0934ec85237da3c64e2e32de8a8
Walter <walterrubicondo@gmail.com>
2021-04-08 17:21:49 +0200

    Fine esercizio

---
+++ main.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+  el: '#root',
+  data: {
+    mail: [],
+  },
+  mounted:
+  function () {
+    for (let i = 0; i < 10; i++) {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+        this.mail.push(response.data.response);
+        console.log(this.mail);
+        });
+      }
+    },
+  methods: {
+    // mailGenerator: function () {
+    //   for (let i = 0; i < 10; i++) {
+    //     axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+    //       this.mail.push(response.data.response);
+    //       console.log(this.mail);
+    //       });
+    //     }
+    //   this.mail = [];
+    // },
+  }
+});
